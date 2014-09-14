becff5d889ca30c2880c1d63a3334db82ecf028e
David English <david@operaonfilm.com>
2014-09-14 08:32:18 -0700

    Corrected the y calculation method

--- app.rb
+++ app.rb
@@ -1,29 +1,43 @@
 class PositionByFrameGen
 
   def initialize(startingCoordinate, endingCoordinate, startingFrame, endingFrame)
+    @TWO = 2
     @startingCoordinate = startingCoordinate
     @endingCoordinate = endingCoordinate
     @startingFrame = startingFrame
     @endingFrame = endingFrame
+    @horizontalCoordinateDifference = (@startingCoordinate[0]).abs + (@endingCoordinate[0]).abs
+    @horizontalHalf = @horizontalCoordinateDifference / @TWO
+    @verticalCoordinateDifference = (@startingCoordinate[1]).abs + (@endingCoordinate[1]).abs
+    @verticalHalf = @verticalCoordinateDifference / @TWO
+    @midpoint = calculateMidpoint()
     @totalNumberOfFrames = @endingFrame - @startingFrame
-    @TWO = 2
-    @amplitude = getHeightDistanceBetweenStartAndEndingCoordinate() / @TWO
   end
 
-  def getHeightDistanceBetweenStartAndEndingCoordinate()
-    opp = (@endingCoordinate[1] - @startingCoordinate[1]).abs
-  end
-
-  def caculatePositionsByFrame
+  def calculateMidpoint()
+    xMidpoint = 0
+    yMidpoint = 0
+    if @startingCoordinate[0] > @endingCoordinate[0]
+      xMidpoint = @startingCoordinate[0] - @horizontalHalf
+    else
+      xMidpoint = @endingCoordinate[0] - @horizontalHalf
+    end
+    if @startingCoordinate[1] > @endingCoordinate[1]
+      yMidpoint = @startingCoordinate[1] - @verticalHalf
+    else
+      yMidpoint = @endingCoordinate[0] - @verticalHalf
+    end
+    [xMidpoint, yMidpoint]
   end
 
   def calculateY(x)
-    @amplitude*Math.sin(((Math::PI/@TWO)/@amplitude)*(x - @amplitude)) + @amplitude
+    #Amplitude * Math.sin((2 * Math::PI / Frequency) * ( x - horizontalOffset)) + verticalOffset
+    @verticalHalf * Math.sin((Math::PI / @horizontalCoordinateDifference) * (x - @midpoint[0])) + @midpoint[1]
   end
 
   def getAllFrameCoordinates()
     arr = []
-    xIncrement = (@endingCoordinate[0] - @startingCoordinate[0]).abs / @totalNumberOfFrames
+    xIncrement = (@endingCoordinate[0] - @startingCoordinate[0].abs).to_f/@totalNumberOfFrames
     currentXCoordinate = @startingCoordinate[0]
     arr.push([@startingFrame, @startingCoordinate])
     ((@startingFrame + 1)..(@endingFrame - 1)).each do |frame|
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,21 +3,37 @@ require_relative '../app'
 describe PositionByFrameGen do
 
   before :all do
-    @testObject = PositionByFrameGen.new([0,0],[10,10],0,10)
+    @testObject001 = PositionByFrameGen.new([0,0],[10,10],0,10)
+    @testObject002 = PositionByFrameGen.new([2,7],[-8,-1],0,10)
+  end
+
+  describe "calculateMidpoint" do
+    it "returns [5,5] when the starting coordinates are [0,0] and the end coordinates are [10,10]" do
+      expect(@testObject001.calculateMidpoint()).to eq([5,5])
+    end
+    it "returns [-3,3] when the starting coordinates are [2,7] and the end coordinates are [-8,-1]" do
+      expect(@testObject002.calculateMidpoint()).to eq([-3,3])
+    end
   end
 
   describe "calculateY" do
     it "returns 0 when x is 0" do
-      expect(@testObject.calculateY(0)).to eq(0)
+      expect(@testObject001.calculateY(0)).to eq(0)
+    end
+    it "returns 10 when x is 10" do
+      expect(@testObject001.calculateY(10)).to eq(10)
     end
     it "returns 5 when x is 5" do
-      expect(@testObject.calculateY(5)).to eq(5)
+      expect(@testObject001.calculateY(5)).to eq(5)
     end
   end
 
   describe "getAllFrameCoordinates" do
-    it "returns an array of each frame and it's coordinates" do
-      expect(@testObject.getAllFrameCoordinates()).to eq([[0, [0, 0]], [1, [1, 0.24471741852423268]], [2, [2, 0.9549150281252627]], [3, [3, 2.061073738537634]], [4, [4, 3.4549150281252627]], [5, [5, 5.0]], [6, [6, 6.545084971874737]], [7, [7, 7.938926261462366]], [8, [8, 9.045084971874736]], [9, [9, 9.755282581475768]], [10, [10, 10]]])
+    it "returns an array of each frame and it's coordinates when the starting coordinates are [0,0] and the end coordinates are [10,10]" do
+      expect(@testObject001.getAllFrameCoordinates()).to eq([[0, [0, 0]], [1, [1, 0.24471741852423268]], [2, [2, 0.9549150281252627]], [3, [3, 2.061073738537634]], [4, [4, 3.4549150281252627]], [5, [5, 5.0]], [6, [6, 6.545084971874737]], [7, [7, 7.938926261462366]], [8, [8, 9.045084971874736]], [9, [9, 9.755282581475768]], [10, [10, 10]]])
+    end
+    it "returns an array of each frame and it's coordinates when the starting coordinates are [2,7] and the end coordinates are [-8,-1]" do
+      expect(@testObject002.getAllFrameCoordinates()).to eq([[0, [2, 7]], [1, [1.0, 6.804226065180615]], [2, [0.0, 6.23606797749979]], [3, [-1.0, 5.3511410091698925]], [4, [-2.0, 4.23606797749979]], [5, [-3.0, 3.0]], [6, [-4.0, 1.7639320225002104]], [7, [-5.0, 0.6488589908301075]], [8, [-6.0, -0.2360679774997898]], [9, [-7.0, -0.8042260651806141]], [10, [-8, -1]]])
     end
   end
 
